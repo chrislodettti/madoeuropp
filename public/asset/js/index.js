ef01ffe1a510feb9956b0ad95009b9c54e4032c6
chrislodettti <cristianlodettialvarez@gmail.com>
2020-06-01 18:42:31 +0200

    first commit

---
+++ public/asset/js/index.js
@@ -0,0 +1,12 @@
+$('#login-button').click(function() {
+    $('.login-form').toggleClass('open');
+})
+$('.menu-trigger').on('click', function() {
+    $('.menu').addClass('slide-in');
+    $('.overlay').removeClass('hide');
+});
+
+$('.menu-close, .overlay, .menu-page-link').on('click', function() {
+    $('.menu').removeClass('slide-in');
+    $('.overlay').addClass('hide');
+});
